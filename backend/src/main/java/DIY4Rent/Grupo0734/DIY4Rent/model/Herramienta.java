572a67888ddddefe041b70754ac4527af337e4b9
Raul <raulsan500@gmail.com>
2024-05-01 08:45:46 +0200

    Añadir Lob a descripcion

--- backend/src/main/java/DIY4Rent/Grupo0734/DIY4Rent/model/Herramienta.java
+++ backend/src/main/java/DIY4Rent/Grupo0734/DIY4Rent/model/Herramienta.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -32,6 +33,7 @@ public class Herramienta {
     @Column(name = "tipo", nullable = false)
     private String tipo;
 
+    @Lob
     @Column(name = "descripcion", nullable = false)
     private String descripcion;
     
